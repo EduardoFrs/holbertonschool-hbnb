ec4c50c45473ae5405c216cc5aa9cb4c6ad64667
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-07 13:02:50 +0200

    mettre en place pydantic pour le BaseModel

--- Model/base_model.py
+++ Model/base_model.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 import uuid
+from pydantic import BaseModel
 
 """
 All the subclasses will inherit from this module/class
 """
+class User(BaseModel):
+    def __init__(self, )
--- Model/cities.py
+++ Model/cities.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+
+from base_model import BaseModel
+
+
+
