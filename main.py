cf6db665b763eef80ec50055dfb5a8818da43242
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-07 11:03:02 +0200

    yes bro ofc bro

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ class User:
 #            raise ValueError("This e-mail adress is already associated to a user.")
 
 class Place:
+    """
+    Place is the superclass
+    """
     def __init__(self, square_meter, max_guests, city, host_username, host_uuid):
         self.square_meter = square_meter
         self.max_guests = max_guests
@@ -23,24 +26,33 @@ class Place:
         self.host_uuid = str(host_uuid.uuid4())
 
 class Appartment(Place):
+    """
+    Subclass of Place
+    """
     def __init__(self, square_meter, max_guests, host_username, host_uuid, room_count):
         super().__init__(square_meter, max_guests, host_username, host_uuid)
         self.room_count = room_count
 
 class Room(Place):
+    """
+    Subclass of Place
+    """
     def __init__(self, square_meter, max_guests, host_username, host_uuid, bed_count):
         super().__init__(square_meter, max_guests, host_username, host_uuid)
         self.bed_count = bed_count
 
 class House(Place):
+    """
+    Subclass of Place
+    """
     def __init__(self, square_meter, max_guests, host_username, host_uuid, floor_number):
         super().__init__(self, square_meter, max_guests, host_username, host_uuid)
         self.floor_number = floor_number
 
 
 class Review:
-    def __init__(self, note)
-    pass
+    def __init__(self, note):
+        self.note = note
 
 class Amenities:
     pass
