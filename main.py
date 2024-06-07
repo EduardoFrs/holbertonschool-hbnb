919a67f7f0624271d0593222427af64ce2f89874
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-07 10:55:07 +0200

    declaring classes/subclasses

--- main.py
+++ main.py
@@ -9,15 +9,37 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
         self.__password = password
+        self.id = str(uuid.uuid4())
 
 #        if self.email is True:
 #            raise ValueError("This e-mail adress is already associated to a user.")
-        self.id = str(uuid.uuid4())
 
 class Place:
-    pass
+    def __init__(self, square_meter, max_guests, city, host_username, host_uuid):
+        self.square_meter = square_meter
+        self.max_guests = max_guests
+        self.city = city
+        self.host_username = host_username
+        self.host_uuid = str(host_uuid.uuid4())
+
+class Appartment(Place):
+    def __init__(self, square_meter, max_guests, host_username, host_uuid, room_count):
+        super().__init__(square_meter, max_guests, host_username, host_uuid)
+        self.room_count = room_count
+
+class Room(Place):
+    def __init__(self, square_meter, max_guests, host_username, host_uuid, bed_count):
+        super().__init__(square_meter, max_guests, host_username, host_uuid)
+        self.bed_count = bed_count
+
+class House(Place):
+    def __init__(self, square_meter, max_guests, host_username, host_uuid, floor_number):
+        super().__init__(self, square_meter, max_guests, host_username, host_uuid)
+        self.floor_number = floor_number
+
 
 class Review:
+    def __init__(self, note)
     pass
 
 class Amenities:
