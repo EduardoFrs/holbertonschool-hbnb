02e2c195b54526696838a4b87e464141b2400091
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-11 09:00:06 +0200

    re organize

--- Model/base_model.py
+++ Model/base_model.py
@@ -1,19 +1,20 @@
-#!/usr/bin/python3
+# """ #!/usr/bin/python3
 
-import uuid
-from dataclasses import dataclass
+# import uuid
+# from dataclasses import dataclass
 
-"""
-All the subclasses will inherit from this module/class
-using dataclasses instead of pydantic
-"""
+# """
+# All the subclasses will inherit from this module/class
+# using dataclasses instead of pydantic
+# """
 
-@dataclass(slots=True)
-class Place:
-    square_meter: float | int
-    max_guests: int
-    city: str
-    country: str
-    host_username: str
-    host_uuid: str
+# @dataclass(slots=True)
+# class Place:
+#     square_meter: float | int
+#     max_guests: int
+#     city: str
+#     country: str
+#     host_username: str
+#     host_uuid: str
 
+#  """
