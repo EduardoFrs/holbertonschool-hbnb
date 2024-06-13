cc29d664f3426cb87580d240993538a2381b0364
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-13 21:35:06 +0200

    created countries.py

---
+++ Model/countries.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from base_model import BaseModel
+
+@dataclass
+class Country(BaseModel):
+    name: str = None
+    _country_code: str = None
+
+
+    """
+    No need for upcoming code
+    -> direct access self.country_code without property def
+
+    @property
+    def country_code(self):
+        return self.__country_code
+
+    To access -> country_object.country_code
+    """
