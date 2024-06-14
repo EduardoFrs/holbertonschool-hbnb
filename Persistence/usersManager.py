3610a79a993dd2e6e9746a843a4e2a00aed9f03c
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-14 13:17:06 +0200

    file for users data management

--- Persistence/usersManager.py
+++ Persistence/usersManager.py
@@ -1,11 +1,27 @@
-import json
+from Persistence.repository import DataManager
+from ..Model.user import User
 
-def save_user(user_object):
-    # takes a user object and converts it to a dict
-    # unset=true avoids including attributes with default value
-    # creates then the users.json
+class UserManager(DataManager):
+    # Manages users data persistence
+    # inherits from DataManager class and assumes user's entities have ID and attr
+    # -> inclus le _save_data du coup toutes les actions sont updatées sur le JSON
+    def __init__(self):
+        super().__init__()
+        self.register_entity_type("user")
 
-    user_data = user_object.dict(exclude_unset=True)
-    with open('users.json', 'w') as outfile:
-        json.dump(user_data,outfile)
+    def get_user(self, user_id: str) -> object:
+        # Retrieves a user based on ID
+        # Args: user_id
+        # Returns: user object if found or None
+        return self.get("user", user_id)
 
+    def save_user(self, user: object) -> bool:
+        # Saves a user obj to data store
+        self._save_data(user)
+        return self.save(user)
+
+    def update_user(self, user: object) -> bool:
+        return self.update(user)
+
+    def delete_user(self, user_id: str) -> bool:
+        return self.delete("user", user_id)
