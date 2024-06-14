a0e35569ab55c3c317b986a04120d92fcb63a6e3
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-14 13:42:25 +0200

    data persistence mgmt for cities, countries and places

---
+++ Persistence/CountriesManager.py
@@ -0,0 +1,18 @@
+from Persistence.repository import DataManager
+
+class CountriesManager(DataManager):
+    def __init__(self):
+        super().__init__()
+        self.register_entity_type("country")
+
+    def get_country(self, country_id: str) -> object:
+        return self.get("country", country_id)
+
+    def save_country(self, country: object) -> bool:
+        return self.save(country)
+
+    def update_country(self, country: object) -> bool:
+        return self.update(country)
+
+    def delete_country(self, country_id: str) -> bool:
+        return self.delete("country", country_id)
---
+++ Persistence/PlacesManager.py
@@ -0,0 +1,18 @@
+from Persistence.repository import DataManager
+
+class PlacesManager(DataManager):
+    def __init__(self):
+        super().__init__()
+        self.register_entity_type("place")
+
+    def get_place(self, place_id: str) -> object:
+        return self.get("place", place_id)
+
+    def save_place(self, place: object) -> bool:
+        return self.save(place)
+
+    def update_place(self, place: object) -> bool:
+        return self.update(place)
+
+    def delete_place(self, place_id: str) -> bool:
+        return self.delete("place", place_id)
