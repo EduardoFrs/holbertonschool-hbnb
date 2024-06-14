c589de0dbb7040219ed5f2313e1d4cdaafe787c0
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-14 08:12:46 +0200

    clean code to implement json to datamanager

--- Persistence/repository.py
+++ Persistence/repository.py
@@ -15,38 +15,19 @@ class IPersistenceManager(ABC):
     """
     @abstractmethod
     def save(self, entity: object):
-        """
-        Saves an entity to persistent storage
-        Args: entity(object)
-        """
         pass
 
     @abstractmethod
     def get(self, entity_id: str, entity_type: str):
-        """
-        Retrieves entity from persistent storage by ID and type
-        Args: ID & type
-        Returns: the entity (object), or None
-        """
         pass
 
     @abstractmethod
     def update(self, entity: object):
-        """
-        Updates an existing entity in persistent storage
-        Args: entity (object)
-        """
         pass
 
     @abstractmethod
     def delete(self, entity_id: str, entity_type: str):
-        """
-        Deletes an entity from persistent storage by its ID / Type
-        Args: ID & type
-        """
         pass
-
-
 
 class DataManager(IPersistenceManager):
     """
@@ -58,33 +39,26 @@ class DataManager(IPersistenceManager):
 
 
     def save(self, entity: object):
-        """
-        Gets entity's Type/ID then stores it in dictionary
-        """
+        # Gets entity's Type/ID then stores it in dictionary
+
         entity_type = type(entity).__name__ # Get entity type from oject
         entity_id = getattr(entity, "id") # Assuming entities have an ID attribute
         self._data.setdefault(entity_type, {}).get(entity_id)
 
     def get(self, entity_id: str, entity_type: str):
-        """
-        Retrieve entity based on ID / type
-        """
+        # Retrieve entity based on ID / type
         return self._data.get(entity_type, {}).get(entity_id)
 
     def update(self, entity: object):
-        """
-        Updates an existing entity (if found)
-        """
+        # Updates an existing entity (if found)
         entity_type = type(entity).__name__
         entity_id = getattr(entity, "id")
         if self.get(entity_id, entity_type):
             self._data[entity_type][entity_id] = entity
 
     def delete(self, entity_id: str, entity_type: str):
-        """
-        Deletes entity based on type/ID
-        Removes the key
-        """
+        # Deletes entity based on type/ID
+        # Removes the key
         if entity_type in self._data:
             del self._data[entity_type][entity_id]
             if not self._data[entity_type]:
